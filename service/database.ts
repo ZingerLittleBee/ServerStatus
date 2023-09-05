bd9ff8de61e6029c586b790ab19404afbe4eac97
ZingerLittleBee <6970999@gmail.com>
2023-09-05 22:37:25 +0800

    feat: 🎸 remove ttl

--- service/database.ts
+++ service/database.ts
@@ -4,22 +4,22 @@ export const db = await Deno.openKv();
 
 export async function loadVersionInfo(): Promise<VersionInfo | null> {
     const result = await db.get(["version"])
-    return result.value ? result.value as VersionInfo : null
+    const value = result.value
+    if (!value || value.lastUpdate < Date.now() - 1000 * 5) return null
+    return value as VersionInfo
 }
 
 export async function saveVersionInfo(version: VersionInfo) {
-    await db.set(["version"], version, {
-        expireIn: 1000
-    })
+    await db.set(["version"], { ...version, lastUpdate: Date.now() })
 }
 
 export async function loadLimitInfo(): Promise<LimitInfo | null> {
     const result = await db.get(["limit"])
-    return result.value ? result.value as LimitInfo : null
+    const value = result.value
+    if (!value || value.lastUpdate < Date.now() - 1000 * 5) return null
+    return value as LimitInfo
 }
 
 export async function saveLimitInfo(limit: LimitInfo) {
-    await db.set(["limit"], limit, {
-        expireIn: 1000
-    })
+    await db.set(["limit"], { ...limit, lastUpdate: Date.now()})
 }
